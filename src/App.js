8a54b533d4a2712658efb4db0e68cf2d896f3fda
pedro-donoso <pedro.donoso.r@usach.cl>
2021-03-13 13:31:33 -0300

    corregido

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
         { id: 1, rating: 3, title: 'El hombre duplicado', image: 'pelicula02.jpg' },
         { id: 2, rating: 5, title: 'El cubo', image: 'pelicula03.jpg' },
         { id: 3, rating: 5, title: 'Cronocrímenes', image: 'pelicula04.jpg' },
-        { id: 4, rating: 5, title: 'Midsomma', image: 'pelicula05.jpg' },
+        { id: 4, rating: 5, title: 'Midsommar', image: 'pelicula05.jpg' },
       ]
       
     };
